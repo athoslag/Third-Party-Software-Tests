47a3674511b502a230e654aaeb4712e3d7395401
Unknown <gunter.mhertz@gmail.com>
2018-05-01 12:03:15 -0300

    Refactored the rubric test class.

--- TCPFinal/UltimoFinal/src/tests/RubricTest.java
+++ TCPFinal/UltimoFinal/src/tests/RubricTest.java
@@ -1,13 +1,18 @@
 package tests;
 
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.*;
-import static org.junit.Assert.*; 
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import implementation.Rubric;
 import interfaces.BudgetItem;
+import interfaces.BudgetItemAnnualValues;
 import tests.TestsHelper;
 
 /**
@@ -27,7 +32,54 @@ public class RubricTest {
 		secondChild = helper.initializeRubric(13, "Second Child", "1.2");
 		grandchildRubric = helper.initializeRubric(123, "Grandchild Rubric", "1.1.1");
 	}
+	
+	@Test
+	public void testRubric() {
+		assertNotNull("Initializer created child budget items correctly", this.rubric.getChildBudgetItems());
+		assertNotNull("Initializer created annual projections correctly", this.rubric.getAnnualProjections());
+	}
 
+	@Test
+	public void testGetName() {
+		assertEquals("Test Rubric", this.rubric.getName());	
+	}
+	
+	@Test
+	public void testGetCode() {
+		assertEquals(1, this.rubric.getCode());
+	}
+	
+	@Test
+	public void testIsCost() {
+		this.rubric.setCost(true);		
+		assertEquals(true, this.rubric.isCost());
+		
+		this.rubric.setCost(false);
+		assertEquals(false, this.rubric.isCost());
+	}
+	
+	@Test
+	public void testSetAnnualProjections() {
+		HashMap<Integer, BudgetItemAnnualValues> annualProj = new HashMap<Integer, BudgetItemAnnualValues>();
+		this.rubric.setAnnualProjections(annualProj);
+		
+		assertEquals(annualProj, this.rubric.getAnnualProjections());
+	}
+	
+	@Test
+	public void testSetChildBudget() {
+		List<BudgetItem> childBudget = new ArrayList<>();
+		this.rubric.setChildBudgetItems(childBudget);
+		
+		assertEquals(childBudget, this.rubric.getChildBudgetItems());
+	}
+	
+	@Test
+	public void testGetFormula() {
+		this.rubric.setFormula("teste");
+		assertEquals("teste", this.rubric.getFormula());
+	}
+	
 	@Test
 	public void testAddChildBudgetItem() {
 		List<BudgetItem> childrenList = new ArrayList<>();
@@ -43,6 +95,10 @@ public class RubricTest {
 
 	@Test
 	public void testIsInBudgetItemSubtree() {
+		//problem with test
+		assertEquals(false, this.rubric.isInBudgetItemSubtree(childRubric));
+		
+		//check that
 		createsKinships();
 		assertTrue("Is child", childRubric.isInBudgetItemSubtree(rubric));
 		assertTrue("Is grandchild", grandchildRubric.isInBudgetItemSubtree(rubric));
